5d9756948bb4935fb64b7afee9a9d09c55bf9662
Swati Singhal <Swati@Swatis-MacBook-Pro.local>
2017-05-09 20:29:38 -0400

    Updated RANSAC

--- Code/ObjectDetectionRNSAC.m
+++ Code/ObjectDetectionRNSAC.m
@@ -1,43 +1,56 @@
 function obPC = ObjectDetectionRNSAC(pts, t, maxIt, IR)  
    iter = 1;
-   InlinerRatio = IR +5;
-   while iter < maxIt && InlinerRatio >IR
-       id = randperm(length(pts),3);
+   inliners1 = [];
+   InlinerRatio = 1;
+   len = length(pts);
+    while iter < maxIt && InlinerRatio > IR && length(pts) > 3    
+      id = randperm(length(pts),3);
        p1 = pts(id(1),:); 
        p2 = pts(id(2),:); 
        p3 = pts(id(3),:); 
-       
+      
        AB = p2 - p1;
        AC = p3 - p1;
        N = cross(AB, AC);
        d = -p1*N';
        F = [ N, d];
-       inliners = [];
+       P = pts*N'+ d;
+       P = abs(P./norm(N));
        count = 0;
+       inliners = [];
+      
        for i = 1:length(pts)
-           P = [pts(i,:) 1];
-           FX = F*P';
-           FX = abs(FX/norm(N));
-           if( FX <= t)
+           FX = P(i,:);
+           if(FX > t)
                inliners = [inliners; i];
                count = count + 1;
-           end        
+                
+           end    
+       end  
+       if len > length(inliners)
+           inliners1 = inliners;
+           len = length(inliners);
        end    
-       InlinerRatio = count/length(pts);
+       InlinerRatio =  count/length(pts);
        iter = iter + 1;
-   end    
-   obPC = inliners;
-   %obPC = post_processing(inliners, pts);
+    end
+    %obPC = inliners1;
+    obPC = post_processing(inliners1, pts);
 end
 
 
 function OB = post_processing(inliners, pts)
     OB = [];
-    mean = sum(inliners)/ size(inliners, 1);
-    for i = 1:length(pts)
-        distance = norm(pts(i,:) - mean);
-        if distance < 0.2 * length(pts)
-            OB = [OB; pts(i,:)];
+    if ~isempty(inliners)
+     minpts = sort(pts(inliners,:));
+     maxD = minpts(1,:) - minpts(end,:);
+     maxD = sqrt(sum(maxD.^2));
+     mean = sum(pts(inliners,:))/ size(inliners, 1);
+     for i = 1:length(inliners)
+        distance = norm(pts(inliners(i),:) - mean);
+        if distance< 0.4* maxD
+            OB = [OB; inliners(i)];
         end    
-    end    
+     end
+    end  
 end
--- Code/Starter.m
+++ Code/Starter.m
@@ -12,17 +12,25 @@ Path = '../Dataset/SingleObject/';
 
 singleScenes = [0, 1 , 2, 6, 8, 12, 22, 23];
 FrameN = [390, 35, 376, 414, 460, 373, 373, 385];
-
-for SceneNum = 2: length(singleScenes) 
+ImName = {'frame', 'image','frame','frame','frame','frame','frame','frame'};
+TH = [40,40,40,40,40,40,40,40];
+InR = [0.2,0.2,0.20,0.20,0.2,0.2,0.2,0.2];
+for SceneNum = 4: length(singleScenes) 
  SceneName = sprintf('%0.3d', singleScenes(SceneNum));
  Points = [];
  r1 = [];
  g1 = [];
  b1 = [];
- for i = 1:FrameN(SceneNum)
-  FrameNum = num2str(1);
-  I = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_rgb.png']);
-  ID = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_depth.png']);
+ Opoints = [];
+ Or = [];
+ Og = [];
+ Ob = [];
+ %for i = 1:FrameN(SceneNum)
+ %
+ for i = 1:16    
+  FrameNum = num2str(i);
+  I = imread([Path,'scene_',SceneName,'/frames/',ImName{SceneNum},'_',FrameNum,'_rgb.png']);
+  ID = imread([Path,'scene_',SceneName,'/frames/',ImName{SceneNum},'_',FrameNum,'_depth.png']);
 
 %% Extract 3D Point cloud
 % Inputs:
@@ -43,20 +51,52 @@ for SceneNum = 2: length(singleScenes)
 % - pcy equals to Y(validInd)
 
  [pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(ID, I, './params/calib_xtion.mat');
- Pts = [pcx pcy pcz];
+  Pts = [pcx pcy pcz];
+  Opoints = [Opoints; Pts];
+  Or= [Or; r];
+  Og= [Og; g];
+  Ob= [Ob; b];
+%  [~,X] = sort(Pts(:,3));
+%  Pts = Pts(X,:);
  maxIter = 100;
- T = 30;
- IR = 0.9;
- ObjCld{SceneNum,i} = ObjectDetectionRNSAC(Pts(1:1000,:), T, maxIter, IR);
- Points = [Points; Pts(cell2mat(ObjCld(SceneNum)),:)];
- r1 = [r1; r(cell2mat(ObjCld(SceneNum)))];
- g1 = [g1; g(cell2mat(ObjCld(SceneNum)))];
- b1 = [b1; g(cell2mat(ObjCld(SceneNum)))];
+ T = TH(SceneNum); %20
+ IR = InR(SceneNum);
+ M = ObjectDetectionRNSAC(Pts(:,:), T, maxIter, IR);
+ if ~isempty(M) 
+   P = Pts(M,:);
+   r2 =  r(M);
+   g2 =  g(M);
+   b2 =  b(M);
+   figure,
+ pcshow(P,[r2 g2 b2]/255);
+ drawnow;
+ title('3D Point Cloud');
+ else 
+    P = [];
+    r2 =[];
+    g2 =[];
+    b2 = [];
+    continue;
+ end  
+
+  ObjCld{SceneNum,i} =  ObjectDetectionRNSAC(P(:,:), T, maxIter, IR);
+  if ~isempty(ObjCld{SceneNum,i})    
+   Points = [Points; P(cell2mat(ObjCld(SceneNum,i)),:)];
+   r1 = [r1; r2(cell2mat(ObjCld(SceneNum,i)))];
+   g1 = [g1; g2(cell2mat(ObjCld(SceneNum,i)))];
+   b1 = [b1; b2(cell2mat(ObjCld(SceneNum,i)))];
+   
+   pcshow(Points,[r1 g1 b1]/255);
+   drawnow;
+   title('3D Point Cloud');
+  end
  end
- save 'state';
 %% Display Images and 3D Points
 % Note this needs the computer vision toolbox: you'll have to run this on
 % the server
+
+
+ close all;
  figure,
  subplot 121
  imshow(I);
@@ -67,6 +107,11 @@ for SceneNum = 2: length(singleScenes)
  
  figure,
  pcshow(Points,[r1 g1 b1]/255);
+ drawnow;
+ title('3D Point Cloud');
+ 
+  figure,
+ pcshow(Opoints,[Or Og Ob]/255);
  drawnow;
  title('3D Point Cloud');
 end
